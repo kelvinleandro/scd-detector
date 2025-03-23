a86dd0806430dbb2363c5d1c2004ec9facd99324
kelvinleandro <kelvinleandro5@gmail.com>
2025-03-23 20:33:16 -0300

    feat(resnet): add new params to apply grid search with different batch sizes

--- grid_search_imle.py
+++ grid_search_imle.py
@@ -31,30 +31,51 @@ data_split_result = main_preprocessing.run_preprocessing_steps(
     preprocess_conf,
     compare=False,
     data_path="music_preprocessed_10s_hotencodeFalse_standard",
-    # data_path="music_preprocessed_10s_standard",
 )
+data_split_result["y_train"] = data_split_result["y_train"].reshape(-1, 1)
+data_split_result["y_val"] = data_split_result["y_val"].reshape(-1, 1)
+data_split_result["y_test"] = data_split_result["y_test"].reshape(-1, 1)
 
-
-slices = [64, 128]
-blocks_list_opts = [[2, 2], [2, 2, 2], [3, 4, 3], [2, 2, 2, 2]]
-start_filters_opts = [16, 32]
+# slices = [64, 128]
+# blocks_list_opts = [[2, 2], [2, 2, 2], [3, 4, 3], [2, 2, 2, 2]]
+blocks_list_opts = [[2], [2, 2], [2, 2, 2]]
+# start_filters_opts = [16, 32]
 dropout_values = [0.5, 0.8]
 lr_opts = [0.01, 0.001, 0.0001]
 
-combinations = product(
-    slices, blocks_list_opts, start_filters_opts, dropout_values, lr_opts
+bs_opts = [256, 512]
+kernel_size_opts = [8, 16]
+
+
+# combinations = list(
+#     product(slices, blocks_list_opts, start_filters_opts, dropout_values, lr_opts)
+# )
+combinations = list(
+    product(bs_opts, blocks_list_opts, kernel_size_opts, dropout_values, lr_opts)
 )
 
 print(f"Total combinations: {len(combinations)}")
-for idx, (beat_len, num_blocks_list, start_filters, dropout, lr) in enumerate(
+# for idx, (beat_len, num_blocks_list, start_filters, dropout, lr) in enumerate(
+#     combinations
+# ):
+for idx, (batch_size, num_blocks_list, kernel_size, dropout, lr) in enumerate(
     combinations
 ):
     print(f"Starting combination {idx+1}/{len(combinations)}")
     try:
-        imlenet_conf.beat_len = beat_len
-        imlenet_conf.start_filters = start_filters
+        imlenet_conf = imlenet_config.Config()
+        transfer_learning_conf = transfer_learning_config.Config()
+        transfer_learning_conf.batch_size = imlenet_conf.batch_size
+        imlenet_conf.decision_threshold = transfer_learning_conf.decision_threshold
+
+        imlenet_conf.batch_size = batch_size
+        imlenet_conf.kernel_size = kernel_size
+        imlenet_conf.num_blocks_list = num_blocks_list
         imlenet_conf.dropout_rate = dropout
         imlenet_conf.learning_rate = lr
+        imlenet_conf.lstm_units = int(
+            imlenet_conf.start_filters * (2 ** (len(imlenet_conf.num_blocks_list) - 2))
+        )
 
         train_gen = DataGen(
             data_split_result["x_train"],
@@ -94,5 +115,6 @@ for idx, (beat_len, num_blocks_list, start_filters, dropout, lr) in enumerate(
             transfer_learning_conf=transfer_learning_conf,
             transfer_learning=run_transfer_learning,
         )
-    except:
+    except Exception as e:
         print(f"Combination {idx+1}/{len(combinations)} failed.")
+        print(e)
